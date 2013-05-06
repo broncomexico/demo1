f654d3e2bac218c6c0c3e5fca547d985ee31e7d3
broncomexico <mcginnis@mcginniss-MacBook-Air.local>
2013-05-06 10:45:51 -0700

    simple express template

--- demo1.js
+++
@@ -1,9 +0,0 @@
-var http = require('http')
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type' : 'text/plain'});
-    res.end("Hello World!\n");
-});
-
-server.listen(1000, '127.0.0.1');
-
-console.log('web server started');
